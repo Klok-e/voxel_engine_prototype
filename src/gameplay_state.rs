9fc5f7b86df41c957b6de773b06707e211c06514
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2020-08-02 15:43:41 +0300

    start fix dimensions

--- src/gameplay_state.rs
+++ src/gameplay_state.rs
@@ -8,6 +8,8 @@ use amethyst::{
 };
 
 use crate::{camera_move_system::init_camera, ui::init_fps_counter, voxels::create_cube};
+use amethyst::input::StringBindings;
+use amethyst::window::ScreenDimensions;
 
 pub struct GameplayState {}
 
@@ -18,5 +20,29 @@ impl SimpleState for GameplayState {
         let mut transform = Transform::default();
         transform.set_translation_xyz(0., 0., 0.);
         create_cube(data.world, transform);
+    }
+
+    fn handle_event(
+        &mut self,
+        data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        use amethyst::winit::{Event, WindowEvent};
+        match event {
+            StateEvent::Window(Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            }) => {
+                let dim = data.world.try_fetch_mut::<ScreenDimensions>();
+                dim.map(|mut dim| {
+                    let new_dim: (f64, f64) = size.into();
+                    dim.update(new_dim.0,new_dim.1);
+                }).unwrap_or_else(||{
+                    log::
+                });
+            }
+            _ => {}
+        }
+        SimpleTrans::None
     }
 }
