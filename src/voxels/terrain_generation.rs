fa66aee72e54f5d1fc08dad42f277f88fec3ec97
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2021-01-24 14:24:15 +0200

    move noise invocation from the innermost loop

--- src/voxels/terrain_generation.rs
+++ src/voxels/terrain_generation.rs
@@ -1,5 +1,5 @@
 use super::{chunk::ChunkPosition, voxel::Voxel};
-use crate::core::{to_vecf, Vec3i};
+use crate::core::{to_vec2d, Vec2i};
 use ndarray::prelude::*;
 use noise::{Fbm, NoiseFn, Seedable};
 
@@ -31,13 +31,14 @@ impl<const N: usize> VoxelGenerator<N> for ProceduralGenerator<N> {
     fn fill_random(&self, pos: &ChunkPosition, arr: &mut ArrayViewMut3<Voxel>) {
         //let mut filled = 0;
         for x in 0..Self::NI {
-            for y in 0..Self::NI {
-                for z in 0..Self::NI {
-                    let p = Vec3i::from([x, y, z]);
-                    let p = to_vecf(p + pos.pos * Self::NI);
-                    let value = self.rng.get([p.x as f64 / 100., p.z as f64 / 100.]);
+            for z in 0..Self::NI {
+                let p = Vec2i::from([x, z]);
+                let p = to_vec2d(p + pos.pos.xz() * Self::NI);
+                let value = self.rng.get([p.x / 100., p.y / 100.]);
+                for y in 0..Self::NI {
+                    let height = y + pos.pos[0] * Self::NI;
                     arr[(x as usize, y as usize, z as usize)] = match value {
-                        value if p.y as f64 + 5. > value * 10. => Voxel { id: 0 },
+                        value if height as f64 + 5. > value * 10. => Voxel { id: 0 },
                         _ => {
                             //filled += 1;
                             Voxel { id: 1 }
