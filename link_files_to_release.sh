14090d1094d6ac2013c6dbfc1fe15ec54f1216b0
LokiVKlokeNaAndoke <ma.ba1@rambler.ru>
2021-02-10 22:57:10 +0200

    fix tasks

--- link_files_to_release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ln -s $(pwd)/assets/ ./target/release/assets
-ln -s $(pwd)/config/ ./target/release/config
-
-ln -s $(pwd)/assets/ ./target/debug/assets
-ln -s $(pwd)/config/ ./target/debug/config
